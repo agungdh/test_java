8f65408a9fb3c4365dc50f834b7e3aa69e6b94a9
AgungDH <agungdh@live.com>
2019-05-14 09:47:31 +0700

    db

---
+++ bin/db.sql
@@ -0,0 +1,88 @@
+-- mysqldump-php https://github.com/ifsnop/mysqldump-php
+--
+-- Host: 127.0.0.1	Database: cilara_test
+-- ------------------------------------------------------
+-- Server version 	5.5.5-10.1.38-MariaDB
+-- Date: Tue, 14 May 2019 04:47:26 +0200
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `barang`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_orang` int(11) NOT NULL,
+  `nama` varchar(191) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_orang` (`id_orang`),
+  CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_orang`) REFERENCES `orang` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `barang`
+--
+
+LOCK TABLES `barang` WRITE;
+/*!40000 ALTER TABLE `barang` DISABLE KEYS */;
+SET autocommit=0;
+INSERT INTO `barang` VALUES (1,1,'HP'),(2,1,'Leptop'),(3,2,'Motor'),(8,1,'Mobel'),(10,2,'zzz');
+/*!40000 ALTER TABLE `barang` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+
+-- Dumped table `barang` with 5 row(s)
+--
+
+--
+-- Table structure for table `orang`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orang` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(191) NOT NULL,
+  `alamat` varchar(191) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orang`
+--
+
+LOCK TABLES `orang` WRITE;
+/*!40000 ALTER TABLE `orang` DISABLE KEYS */;
+SET autocommit=0;
+INSERT INTO `orang` VALUES (1,'AgungDH','NGR'),(2,'Bintang','PSW'),(41,'12','2'),(44,'aa','sf');
+/*!40000 ALTER TABLE `orang` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+
+-- Dumped table `orang` with 4 row(s)
+--
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on: Tue, 14 May 2019 04:47:26 +0200
